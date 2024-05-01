ad9eb1c3f613956e4360613e8b8b3042abe80b3f
Kevin <kevin@metasismedia.com>
2024-05-01 14:37:08 +0000

    cool

--- app/Http/Controllers/FournisseursController.php
+++ app/Http/Controllers/FournisseursController.php
@@ -273,12 +273,12 @@ class FournisseursController extends Controller
         $result = livraisonCommande::join('livraisons AS l', 'livraison_commandes.livraison_id', '=', 'l.id')
             ->where('l.boutique_id', Auth::user()->boutique_id)
             ->whereColumn('livraison_commandes.quantite_vendue', '<', 'livraison_commandes.quantite_livre')
-            ->select('livraison_modeles.modele') // Extraction du champ 'modele'
-            ->groupBy('livraison_modeles.modele')
+            ->select('livraison_modeles.modele_id') // Extraction du champ 'modele'
+            ->groupBy('livraison_modeles.modele_id')
             ->get();
 
         // Obtenir les IDs des modèles
-        $modeles_ids = $result->pluck('modele'); // Pluck 'modele' from $result
+        $modeles_ids = $result->pluck('modele_id'); // Pluck 'modele' from $result
 
         // Filtrer $modele en utilisant whereIn()
         $modele = DB::table('modeles')
