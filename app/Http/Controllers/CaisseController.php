f445590451b9d3f1edbd38cd384544b29723cd13
devBonkey <fbonkey22@gmail.com>
2024-05-04 13:51:35 +0000

    boutique

--- app/Http/Controllers/CaisseController.php
+++ app/Http/Controllers/CaisseController.php
@@ -109,6 +109,7 @@ class CaisseController extends Controller
                     ->leftJoin('depenses', 'depenses.boutique_id', '=', 'boutiques.id')
                     ->selectRaw('
                         caisses.id,
+                        caisses.date,
                         boutiques.nom AS boutique_nom,
                         SUM(ventes.totaux) AS totalVente,
                         SUM(ventes.totaux - ventes.montant_reduction) AS VenteNette,
