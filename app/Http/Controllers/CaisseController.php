4d0322208c42204ced8885b90eb04526e2e6e9e2
Kevin <kevin@metasismedia.com>
2024-05-04 01:09:06 +0000

    total avoirs

--- app/Http/Controllers/CaisseController.php
+++ app/Http/Controllers/CaisseController.php
@@ -102,34 +102,43 @@ class CaisseController extends Controller
 
             if (count($records) > 0) {
                 $caisse = DB::table('boutiques')
-                ->join('caisses', function ($join) {
-                    $join->on('caisses.boutique_id', '=', 'boutiques.id');
-                })
-                ->join('avoirs', function($join){
-                    $join->on('avoirs.boutique_id', '=', 'boutiques.id');
-                })
-                ->where('caisses.boutique_id',Auth::user()->boutique->id)
-                ->where('avoirs.boutique_id',Auth::user()->boutique->id)
-                ->select('caisses.*','boutiques.*')
-                ->orderBy('caisses.date','desc')
-                ->get();
-                $global=   DB::table('boutiques')
-                ->join('ventes', function ($join) {
-                    $join->on('ventes.boutique_id', '=', 'boutiques.id');
-                })
-                ->join('reglements', function ($join) {
-                    $join->on('reglements.vente_id', '=', 'ventes.id');
-                })
-                ->join('depenses', function ($join) {
-                    $join->on('depenses.boutique_id', '=', 'boutiques.id');
-                })
+                    ->join('caisses', function ($join) {
+                        $join->on('caisses.boutique_id', '=', 'boutiques.id');
+                    })
+                    ->join('avoirs', function($join){
+                        $join->on('avoirs.boutique_id', '=', 'boutiques.id');
+                    })
+                    ->where('caisses.boutique_id', Auth::user()->boutique->id)
+                    ->where('avoirs.boutique_id', Auth::user()->boutique->id)
+                    ->select('caisses.*', 'boutiques.*')
+                    ->orderBy('caisses.date', 'desc')
+                    ->get();
 
-                ->selectRaw('sum(ventes.totaux) as totalVente ,sum(ventes.totaux - ventes.montant_reduction ) as VenteNette, sum(ventes.montant_reduction) as totalReduction,
-                sum(reglements.montant_donne) as totalReglement ,depenses.date_dep, sum(depenses.montant) as totalDepense , boutiques.nom as boutique, sum(avoirs.amount) as totalAvoirs')
+                $global = DB::table('boutiques')
+                    ->join('ventes', function ($join) {
+                        $join->on('ventes.boutique_id', '=', 'boutiques.id');
+                    })
+                    ->join('reglements', function ($join) {
+                        $join->on('reglements.vente_id', '=', 'ventes.id');
+                    })
+                    ->join('depenses', function ($join) {
+                        $join->on('depenses.boutique_id', '=', 'boutiques.id');
+                    })
+                    ->join('avoirs', function ($join) {
+                        $join->on('avoirs.boutique_id', '=', 'boutiques.id');
+                    })
+                    ->selectRaw('sum(ventes.totaux) as totalVente,
+                                sum(ventes.totaux - ventes.montant_reduction) as VenteNette,
+                                sum(ventes.montant_reduction) as totalReduction,
+                                sum(reglements.montant_donne) as totalReglement,
+                                depenses.date_dep,
+                                sum(depenses.montant) as totalDepense,
+                                boutiques.nom as boutique,
+                                sum(avoirs.amount) as totalAvoirs')
+                    ->groupBy('depenses.date_dep', 'boutiques.id', 'boutiques.nom')
+                    ->orderBy('depenses.date_dep', 'desc', 'boutiques.id', 'desc')
+                    ->get();
 
-                ->groupBy('depenses.date_dep','boutiques.id','boutiques.nom')
-                ->orderBy('depenses.date_dep', 'desc','boutiques.id', 'desc')
-                ->get();
             } else {
                 return view('caisse.listeglobal');
                   }
