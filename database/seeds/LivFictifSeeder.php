d64ce26bf070791cc6452e052cb93181be262f59
Kevin <kevin@metasismedia.com>
2024-05-05 03:05:39 +0000

    cool stuff

--- database/seeds/LivFictifSeeder.php
+++ database/seeds/LivFictifSeeder.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use App\Modele;
 use App\Livraison;
+use App\Boutique;
 use App\livraisonCommande;
 
 class LivFictifSeeder extends Seeder
@@ -16,32 +17,38 @@ class LivFictifSeeder extends Seeder
     public function run()
     {
         $modeles = Modele::all();
-        foreach($modeles as $modele){
-            
+        $boutiques = Boutique::where('id', '!=', 1)->get();
+
+        foreach($boutiques as $boutique){
             $id=DB::table('livraisons')->max('id');
             $ed=1+$id;
             $livraison = new Livraison();
             $livraison ->numero="LIV".now()->format('Y')."-".$ed;
             $livraison ->date_livraison= now();
-            $livraison ->boutique_id=$modele->boutique_id;
+            $livraison ->boutique_id=$boutique->id;
+            $livraison->statut = 1;
             $livraison->save();
 
-            $livraisonCom = new livraisonCommande();
-            $livraisonCom ->livraison_id=$livraison->id;
-            $livraisonCom->quantite_livre = $modele->quantite;
-            $livraisonCom->quantite_restante = 0;
-            $livraisonCom->quantite_vendue = 0;
-            $livraisonCom->livraison_id = $livraison->id;
-            $livraisonCom->modele_id = $modele->id;
+            foreach($modeles as $modele){
+                if($modele->boutique_id == $boutique->id && $modele->quantite>0){
+                    $livraisonCom = new livraisonCommande();
+                    $livraisonCom ->livraison_id=$livraison->id;
+                    $livraisonCom->quantite_livre = $modele->quantite;
+                    $livraisonCom->quantite_restante = 0;
+                    $livraisonCom->quantite_vendue = 0;
+                    $livraisonCom->livraison_id = $livraison->id;
+                    $livraisonCom->modele_id = $modele->id;
 
-            $my_modele = Modele::where('libelle', $modele->libelle)->where('boutique_id', 1)->get()->first();
-            $livraisonCom->commande_modele_id = $my_modele->id;
-            $livraisonCom->save();
+                    $my_modele = Modele::where('libelle', $modele->libelle)->where('boutique_id', $boutique->id)->get()->first();
+                    $livraisonCom->commande_modele_id = $my_modele->id;
+                    $livraisonCom->save();
 
-            $my_modele->quantite -= $modele->quantite;
-            $my_modele->save();
+                    $my_modele->quantite -= $modele->quantite;
+                    $my_modele->save();
+                }
+            }
         }
-
+        
 
     }
 }
