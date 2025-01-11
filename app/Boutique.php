9df421fa961c9fa1c83480f1d8c3c3a85cc8412d
kevin <87210876+Kevram73@users.noreply.github.com>
2025-01-11 21:08:22 +0000

    cool history

--- app/Boutique.php
+++ app/Boutique.php
@@ -50,4 +50,21 @@ class Boutique extends Model
     {
         return $this->hasManyThrough(Reglement::class, Client::class);
     }
+
+    public function getValueAttribute($value)
+    {
+        return $this->formatAmount($value);
+    }
+
+    // Méthode pour formater les montants
+    protected function formatAmount($value)
+    {
+        // Vérifie si la valeur est numérique avant de la formater
+        if (is_numeric($value)) {
+            return number_format($value, 2, ',', ' ');
+        }
+
+        // Retourne la valeur brute si ce n'est pas un nombre
+        return $value;
+    }
 }
